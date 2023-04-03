3f2550cbbe7a21b7b256cc31a1018a1995f43805
emrrham <mremir255@gmail.com>
2023-04-03 16:38:13 +0300

    starr

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Basamak sayısını girin!");
+        int k = input.nextInt();
+        for (int j = 1;j<=(j - 1);j++){
+            for (int m = 1; m<=k; m++){
+
+        }
+        int star = (k+1) - j;
+            System.out.println(2*star-1);
+            System.out.println();
+        }
+
+
+
+    }
+}
